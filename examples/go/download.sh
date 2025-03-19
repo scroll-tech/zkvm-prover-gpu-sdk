3f16cdafe708791c8da40d94040e80bd4e2eb99e
kunxian xia <xiakunxian130@gmail.com>
2025-03-19 14:28:51 +0800

    update openvm.toml

--- examples/go/download.sh
+++ examples/go/download.sh
@@ -1,8 +1,15 @@
 
-SCROLL_ZKVM_VERSION=0.1.0-rc.8
+# release version
+if [ -z "${SCROLL_ZKVM_VERSION}" ]; then
+  echo "SCROLL_ZKVM_VERSION not set"
+  exit 1
+fi
 
 wget https://circuit-release.s3.us-west-2.amazonaws.com/scroll-zkvm/releases/$SCROLL_ZKVM_VERSION/chunk/app.vmexe -O ./assets/chunk/app.vmexe
+wget https://circuit-release.s3.us-west-2.amazonaws.com/scroll-zkvm/releases/$SCROLL_ZKVM_VERSION/chunk/openvm.toml -O ./assets/chunk/openvm.toml
 
 wget https://circuit-release.s3.us-west-2.amazonaws.com/scroll-zkvm/releases/$SCROLL_ZKVM_VERSION/batch/app.vmexe -O ./assets/batch/app.vmexe
+wget https://circuit-release.s3.us-west-2.amazonaws.com/scroll-zkvm/releases/$SCROLL_ZKVM_VERSION/batch/openvm.toml -O ./assets/batch/openvm.toml
 
 wget https://circuit-release.s3.us-west-2.amazonaws.com/scroll-zkvm/releases/$SCROLL_ZKVM_VERSION/bundle/app.vmexe -O ./assets/bundle/app.vmexe
+wget https://circuit-release.s3.us-west-2.amazonaws.com/scroll-zkvm/releases/$SCROLL_ZKVM_VERSION/bundle/openvm.toml -O ./assets/bundle/openvm.toml
