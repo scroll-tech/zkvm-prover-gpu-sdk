69f3d01639c5a2b3c5497788120d237304bb1a1c
Morty <yiweichi1@gmail.com>
2025-03-19 23:29:07 +0800

    update zkvm interface

--- interface/zkvm.h
+++ interface/zkvm.h
@@ -2,20 +2,13 @@
 void init(char* config);
 
 // to generate proofs, proofs are encoded in json
-char* generate_chunk_proof(const char* input);
+char* generate_chunk_proof(const char* input, char* fork_name);
 
-char* generate_batch_proof(const char* input);
+char* generate_batch_proof(const char* input, char* fork_name);
 
-char* generate_bundle_proof(const char* input);
+char* generate_bundle_proof(const char* input, char* fork_name);
 
 void free_proof(char* proof);
-
-// to verify proofs
-char verify_chunk_proof(char* proof);
-
-char verify_batch_proof(char* proof);
-
-char verify_bundle_proof(char* proof);
 
 // vks are encoded in base64
 char* get_chunk_vk();
