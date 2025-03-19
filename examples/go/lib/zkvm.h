c230b8f33c6e87aa7260ec8f8190420bc80e40a6
Morty <yiweichi1@gmail.com>
2025-03-20 00:09:43 +0800

    update euclid_prover commit

--- examples/go/lib/zkvm.h
+++ examples/go/lib/zkvm.h
@@ -1,20 +1,20 @@
-// TODO: add comments
+// initialize the library
 void init(char* config);
 
-// TODO: add comments
-// proofs are encoded in json
-char* generate_chunk_proof(const char* input, char* fork_name);
+// to generate proofs, proofs are encoded in json
+char* generate_chunk_proof(const char* input);
 
-char* generate_batch_proof(const char* input, char* fork_name);
+char* generate_batch_proof(const char* input);
 
-char* generate_bundle_proof(const char* input, char* fork_name);
+char* generate_bundle_proof(const char* input);
 
 void free_proof(char* proof);
 
-// TODO: add comments
 // vks are encoded in base64
 char* get_chunk_vk();
+
 char* get_batch_vk();
+
 char* get_bundle_vk();
 
 void free_vk(char* vk);
