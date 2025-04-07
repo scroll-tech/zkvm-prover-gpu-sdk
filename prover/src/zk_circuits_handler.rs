7825c628b4bf1c7bf19018924229af821481726a
Morty <yiweichi1@gmail.com>
2025-04-08 04:17:49 +0800

    fix: new_handler

--- prover/src/zk_circuits_handler.rs
+++ prover/src/zk_circuits_handler.rs
@@ -2,7 +2,7 @@ use anyhow::Result;
 use async_trait::async_trait;
 use prover::ProofType;
 
-pub trait CircuitsHandler: Sync + Send {
+pub trait CircuitsHandler {
     fn get_vk(&self, task_type: ProofType) -> Option<Vec<u8>>;
 
     fn get_proof_data(&self, proof_type: ProofType, input: String, fork_name: String) -> Result<String>;
