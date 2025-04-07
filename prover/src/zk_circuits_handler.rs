e2407e6832600b38c383aafe17a0ba7209715247
Morty <yiweichi1@gmail.com>
2025-04-08 04:07:04 +0800

    fix: trait CircuitsHandler

--- prover/src/zk_circuits_handler.rs
+++ prover/src/zk_circuits_handler.rs
@@ -2,7 +2,7 @@ use anyhow::Result;
 use async_trait::async_trait;
 use prover::ProofType;
 
-pub trait CircuitsHandler {
+pub trait CircuitsHandler: Sync + Send {
     fn get_vk(&self, task_type: ProofType) -> Option<Vec<u8>>;
 
     fn get_proof_data(&self, proof_type: ProofType, input: String, fork_name: String) -> Result<String>;
